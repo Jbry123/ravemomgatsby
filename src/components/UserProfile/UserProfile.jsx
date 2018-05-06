25cb5a846e8c7c4ee6e56e4d611b4d3543150389
Unknown <jonathanbry@comcast.net>
2018-05-06 10:40:20 -0700

    final

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -5,13 +5,13 @@ class UserProfile extends Component {
   render() {
 
     return (
-<div className="FriendBlock-Container">
+<div className="UserProfile-Container">
 <img className="Profile-pic"src="https://i.imgur.com/yZdQCsr.png" height="100" width="100" />
 
-<h4>RaveMomma
-  <p>new message(1):Hey it was great meeting you!</p>
+<h4 className="Userh4">RaveMomma
+  <p>First encounter: You met RaveMomma at EDC!</p>
 </h4>
-<div className="Icon-wrapper">
+<div className="UserIcon-wrapper">
   <img className="Icon-pic" src="http://www.clker.com/cliparts/H/i/N/V/m/o/plus-sign-hi.png" alt="" height="31" width="30"/>
   <img className="Icon-pic" src="http://simpleicon.com/wp-content/uploads/bluetooth.png" alt="" height="32" width="35"/>
 </div>
